ee6d15c7e3c8ed2c7bff77972341ddcc366614cc
Munkh SHUTIS COM <munkh.hustle@gmail.com>
2025-12-05 15:59:02 +0800

    221-2-5

--- src/worker.js
+++ src/worker.js
@@ -2,26 +2,7 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
     
-    // Route to specific pages
-    if (url.pathname === '/' || url.pathname === '/login.html') {
-      return env.ASSETS.fetch(request);
-    } else if (url.pathname === '/login' || url.pathname === '/login.html') {
-      // Modify the request URL to get login.html
-      const newUrl = new URL(request.url);
-      newUrl.pathname = '/login.html';
-      return env.ASSETS.fetch(new Request(newUrl, request));
-    } else if (url.pathname === '/signup' || url.pathname === '/signup.html') {
-      // Modify the request URL to get signup.html
-      const newUrl = new URL(request.url);
-      newUrl.pathname = '/signup.html';
-      return env.ASSETS.fetch(new Request(newUrl, request));
-    } else if (url.pathname === '/style.css') {
-      return env.ASSETS.fetch(request);
-    }
-    
-    // Default: serve the signup page
-    const signupUrl = new URL(request.url);
-    signupUrl.pathname = '/signup.html';
-    return env.ASSETS.fetch(new Request(signupUrl, request));
+    // Let the asset handler serve all files from the public folder
+    return env.ASSETS.fetch(request);
   }
 }

52257826d036f39c7787a1d4005805f5e4754afb
Munkh SHUTIS COM <munkh.hustle@gmail.com>
2025-12-05 16:30:29 +0800

    221-2-10

--- src/worker.js
+++ src/worker.js
@@ -2,10 +2,21 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
     
-    // Handle root path - serve index.html
+    // Clean URL routing
     if (url.pathname === '/') {
-      const response = await env.ASSETS.fetch(new URL(request.url).origin + '/index.html');
-      return response;
+      const newUrl = new URL(request.url);
+      newUrl.pathname = '/index.html';
+      return env.ASSETS.fetch(new Request(newUrl, request));
+    } else if (url.pathname === '/login') {
+      const newUrl = new URL(request.url);
+      newUrl.pathname = '/login.html';
+      return env.ASSETS.fetch(new Request(newUrl, request));
+    } else if (url.pathname === '/signup') {
+      const newUrl = new URL(request.url);
+      newUrl.pathname = '/signup.html';
+      return env.ASSETS.fetch(new Request(newUrl, request));
+    } else if (url.pathname === '/style.css') {
+      return env.ASSETS.fetch(request);
     }
     
     // For all other static files, let ASSETS handle it

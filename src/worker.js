06e2863546dfd8a4de618f90e39a3359571ce0ee
Munkh SHUTIS COM <munkh.hustle@gmail.com>
2025-12-05 15:47:38 +0800

    221-2-1

--- src/worker.js
+++ src/worker.js
@@ -1,5 +1,20 @@
 export default {
   async fetch(request, env) {
-    return new Response("Hello world")
-  }
-}
+    const url = new URL(request.url);
+    const path = url.pathname;
+
+    // Handle different routes
+    if (path === '/' || path === '/index.html') {
+      return serveStaticFile('index.html', 'text/html');
+    } else if (path === '/login' || path === '/login.html') {
+      return serveStaticFile('login.html', 'text/html');
+    } else if (path === '/signup' || path === '/signup.html') {
+      return serveStaticFile('signup.html', 'text/html');
+    } else if (path === '/style.css') {
+      return serveStaticFile('style.css', 'text/css');
+    } else {
+      // Default to login page for any other route
+      return serveStaticFile('login.html', 'text/html');
+    }
+  },
+};
